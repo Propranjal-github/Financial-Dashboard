8aac6d3498c2f4510493929b9fca74af4e93dde7
Propranjal-github <pranjalpathak214@gmail.com>
2026-04-06 02:54:11 +0530

    Added: Allowed_Hosts

--- finance_dashboard/settings.py
+++ finance_dashboard/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-dev')
 
 DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [host.strip() for host in os.environ.get('ALLOWED_HOSTS', '*').split(',') if host.strip()]
 
 # ---------------------------------------------------------------------------
 # Application definition
